ed5bc5caaa0ec122f553b66189c8c27f17f5879e
ronald-van-de-kuil <ronald.van.de.kuil@nl.ibm.com>
2023-10-19 07:43:58 +0200

    first steps with nodejs

---
+++ main.js
@@ -0,0 +1,14 @@
+var app = require('http').createServer(function (request, response) {
+    // print http orignal message like curl
+
+    // request.method, URL and httpVersion
+    console.log(request.method + ' ' + request.url + ' HTTP/' + request.httpVersion);
+
+    // request.headers
+    for (var property in request.headers) {
+        if (request.headers.hasOwnProperty(property)) {
+            console.log(property + ': ' + request.headers[property])
+        }
+    }
+});
+app.listen(3000);   
